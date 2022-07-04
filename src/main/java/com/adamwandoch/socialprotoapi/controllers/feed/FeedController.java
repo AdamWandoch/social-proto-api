f1aef9dad788cd807ab953a4c2f3054836674b85
Adam Wandoch <wandoch.adam@gmail.com>
2022-07-04 23:48:01 +0100

    websocket server integrated successfully

--- src/main/java/com/adamwandoch/socialprotoapi/controllers/feed/FeedController.java
+++ src/main/java/com/adamwandoch/socialprotoapi/controllers/feed/FeedController.java
@@ -1,14 +1,9 @@
 package com.adamwandoch.socialprotoapi.controllers.feed;
 
-import com.adamwandoch.socialprotoapi.models.post.PostModel;
-import com.adamwandoch.socialprotoapi.models.post.PostService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
-import java.util.List;
 
 /**
  * @author Adam Wandoch
@@ -17,15 +12,19 @@ import java.util.List;
 @Controller
 public class FeedController {
 
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
-    @Autowired
-    private PostService postService;
+//    @Autowired
+//    private PostService postService;
+//
+//    @MessageMapping("/update")
+//    @SendTo("feed-update/public")
+//    public List<PostModel> feedUpdate() {
+//        return postService.getAllPosts();
+//    }
 
-    @MessageMapping("/update")
-    @SendTo("feed-update/public")
-    public List<PostModel> feedUpdate() {
-        return postService.getAllPosts();
+    @MessageMapping("/feed-trigger")
+    @SendTo("/feed-clients")
+    public String receiveMessage(@Payload String message) {
+        return message;
     }
 
 }
--- src/main/java/com/adamwandoch/socialprotoapi/controllers/feed/WebSocketConfig.java
+++ src/main/java/com/adamwandoch/socialprotoapi/controllers/feed/WebSocketConfig.java
@@ -20,9 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/feed-update");
-        registry.setUserDestinationPrefix("/user");
+        registry.enableSimpleBroker("/feed-clients");
     }
 }
 
