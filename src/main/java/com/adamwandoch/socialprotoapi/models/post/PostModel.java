59ee253467fd697b1a140af23491970f04ffd70b
Adam Wandoch <wandoch.adam@gmail.com>
2022-07-06 07:10:14 +0100

    added lombok to project to remove some boilerplate code

--- src/main/java/com/adamwandoch/socialprotoapi/models/post/PostModel.java
+++ src/main/java/com/adamwandoch/socialprotoapi/models/post/PostModel.java
@@ -1,17 +1,19 @@
 package com.adamwandoch.socialprotoapi.models.post;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.ArrayList;
-import java.util.Objects;
 
 /**
  * @author Adam Wandoch
  */
 
 @Entity
+@Data
 public class PostModel {
 
     @Id
@@ -24,18 +26,6 @@ public class PostModel {
     @Column(length = 20000)
     private ArrayList<Long> usersThatLiked = new ArrayList<>();
 
-    public PostModel() {
-    }
-
-    public PostModel(Long id, Long userId, String content, String timestamp, int likes, ArrayList<Long> usersThatLiked) {
-        this.id = id;
-        this.userId = userId;
-        this.content = content;
-        this.timestamp = timestamp;
-        this.likes = likes;
-        this.usersThatLiked = usersThatLiked;
-    }
-
     public void like(Long userId) {
         this.likes++;
         this.usersThatLiked.add(userId);
@@ -44,78 +34,4 @@ public class PostModel {
     public void addUserThatLiked(Long userId) {
         this.usersThatLiked.add(userId);
     }
-
-    public int getLikes() {
-        return likes;
-    }
-
-    public void setLikes(int likes) {
-        this.likes = likes;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(String timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public ArrayList<Long> getUsersThatLiked() {
-        return usersThatLiked;
-    }
-
-    public void setUsersThatLiked(ArrayList<Long> usersThatLiked) {
-        this.usersThatLiked = usersThatLiked;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, userId, content, timestamp, likes, usersThatLiked);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PostModel postModel = (PostModel) o;
-        return likes == postModel.likes && Objects.equals(id, postModel.id) && Objects.equals(userId, postModel.userId) && Objects.equals(content, postModel.content) && Objects.equals(timestamp, postModel.timestamp) && Objects.equals(usersThatLiked, postModel.usersThatLiked);
-    }
-
-    @Override
-    public String toString() {
-        return "PostModel{" +
-                "id=" + id +
-                ", userId=" + userId +
-                ", content='" + content + '\'' +
-                ", timestamp='" + timestamp + '\'' +
-                ", likes=" + likes +
-                ", usersThatLiked=" + usersThatLiked +
-                '}';
-    }
-
 }
--- src/main/java/com/adamwandoch/socialprotoapi/models/user/UserModel.java
+++ src/main/java/com/adamwandoch/socialprotoapi/models/user/UserModel.java
@@ -1,15 +1,17 @@
 package com.adamwandoch.socialprotoapi.models.user;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Objects;
 
 /**
  * @author Adam Wandoch
  */
 
 @Entity
+@Data
 public class UserModel {
 
     @Id
@@ -18,58 +20,4 @@ public class UserModel {
     private String nickname;
     private int avatarId;
 
-    public UserModel() {
-    }
-
-    public UserModel(Long id, String nickname, int avatar) {
-        this.id = id;
-        this.nickname = nickname;
-        this.avatarId = avatar;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public int getAvatarId() {
-        return avatarId;
-    }
-
-    public void setAvatarId(int avatarId) {
-        this.avatarId = avatarId;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nickname, avatarId);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        UserModel user = (UserModel) o;
-        return id == user.id && avatarId == user.avatarId && Objects.equals(nickname, user.nickname);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", nickname='" + nickname + '\'' +
-                ", avatar=" + avatarId +
-                '}';
-    }
 }
