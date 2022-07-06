03562631641e71fbee1b983d862ef5d2d5c30aa0
Adam Wandoch <wandoch.adam@gmail.com>
2022-07-06 07:44:34 +0100

    changed FeedController to return up to date array of posts directly instead of triggering a get request on client side

--- src/main/java/com/adamwandoch/socialprotoapi/controllers/feed/FeedController.java
+++ src/main/java/com/adamwandoch/socialprotoapi/controllers/feed/FeedController.java
@@ -1,9 +1,14 @@
 package com.adamwandoch.socialprotoapi.controllers.feed;
 
+import com.adamwandoch.socialprotoapi.models.post.PostModel;
+import com.adamwandoch.socialprotoapi.models.post.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 /**
  * @author Adam Wandoch
@@ -12,10 +17,14 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class FeedController {
 
+    @Autowired
+    private PostService postService;
+
     @MessageMapping("/feed-trigger")
     @SendTo("/feed-clients")
-    public String receiveMessage(@Payload String message) {
-        return message;
+    public List<PostModel> receiveMessage(@Payload String message) throws InterruptedException {
+        Thread.sleep(1000); // allow repository to save new data from client
+        return postService.getAllPosts();
     }
 
 }
